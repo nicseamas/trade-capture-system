3e2956af626657971c16db6e72d2c79e86246611
nicseamas <study9102016@gmail.com>
2025-11-07 20:17:40 +0000

    formated checklist.md

--- backend/src/main/java/com/technicalchallenge/repository/TradeRepository.java
+++ backend/src/main/java/com/technicalchallenge/repository/TradeRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 @Repository
-public interface TradeRepository extends JpaRepository<Trade, Long> {
+public interface TradeRepository extends JpaRepository<Trade, Long>, JpaSpecificationExecutor<Trade> {
     // Existing methods
     List<Trade> findByTradeId(Long tradeId);
 
